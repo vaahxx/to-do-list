ee3a57b067678508da027b80c1950a606dd9dfbd
Valentina Garavaglia <vaahxx@gmail.com>
2022-08-21 11:55:35 -0400

    improvements

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -6,7 +6,6 @@ function Header(props: HeaderProps): JSX.Element {
   return (
     <header>
       <h3>To-do List App</h3>
-      <p>You can create your today's list here</p>
       <p>{`Today, ${props.date.toLocaleDateString()}`}</p>
     </header>
   );
